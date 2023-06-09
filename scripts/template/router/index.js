1b3cd5d9a59773944b8a53f77f5fb8bb2fece72f
jiang-xia <963798512@qq.com>
2023-06-09 15:57:23 +0800

    feat:  模板完善

--- scripts/template/router/index.js
+++ scripts/template/router/index.js
@@ -3,11 +3,20 @@ const router = createRouter({
   history: createWebHashHistory(), // hash模式：createWebHashHistory，history模式：createWebHistory
   routes: [
     {
+      path: '/',
+      redirect: '/home',
+    },
+    {
       path: '/home',
       name: 'index',
       component: () => import('../views/index.vue'),
       meta: { title: '首页' }
-    }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: () => import('@/global/pages/not-found/index.vue'),
+    },
   ]
 })
 
---
+++ src/global/pages/not-found/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="content">
+      <div class="result"> </div>
+      <div class="operation-row">
+        <button @click="back"> back </button>
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+    import { useRouter } from 'vue-router';
+  
+    const router = useRouter();
+    const back = () => {
+      console.log(router.replace)
+      router.replace('/');
+    };
+  </script>
+  
+  <style scoped lang="less">
+    .content {
+      // padding-top: 100px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-left: -95px;
+      margin-top: -121px;
+      text-align: center;
+    }
+  </style>
+  
